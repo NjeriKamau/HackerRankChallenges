8128f44244321c9ee8078aa0436f0129befc69b8
TNjeri <NjeriKamau@users.noreply.github.com>
2018-10-05 11:23:41 +0300

    Create Challenge_Day0.py
    Like they said, You got this.
    All you have to do is print inputString

---
+++ Challenge_Day0.py
@@ -0,0 +1,18 @@
+#Task
+#To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. 
+#on a single line, and finally print the value of your variable on a second line.
+
+#You've got this!
+
+#Note: The instructions are Java-based, but we support submissions in many popular languages. 
+#You can switch languages using the drop-down menu above your editor, 
+#and the variable may be written differently depending on the best-practice conventions of your submission language
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+inputString = raw_input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print 'Hello, World.'
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print inputString
